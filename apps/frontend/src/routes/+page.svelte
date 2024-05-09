aeaf51c63f512140a9d1f5ed3652498e192f1e59
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 12:04:17 -0400

    feat: add navbar buttons to products page header

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { onMount } from 'svelte'
   import { ProductsService } from '@/services/products'
+
+  import NavbarButtons from '@/components/navbar-buttons.svelte'
   import Product from '@/components/product.svelte'
 
   let products: ProductEntity[] = []
@@ -15,6 +17,7 @@
   }
 
   const handleAddProduct = () => {
+    // TODO - Implement add product to cart
     console.log('Product added to cart')
   }
 
@@ -24,12 +27,13 @@
 </script>
 
 <div class="flex flex-col gap-10 p-10">
-  <header>
+  <header class="flex items-center justify-between">
     <h1 class="text-4xl font-bold tracking-wide">
       Products
     </h1>
+    <NavbarButtons/>
   </header>
-  <main class="grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-6">
+  <main class="grid grid-cols-[repeat(auto-fill,20rem)] gap-6">
     {#each products as product}
       <Product
         name={product.name}
