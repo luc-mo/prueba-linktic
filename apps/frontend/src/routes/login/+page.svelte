2b90ae383e475ddc80478593b601da73ca956452
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 08:59:26 -0400

    feat: add login state credentials handling

--- apps/frontend/src/routes/login/+page.svelte
+++ apps/frontend/src/routes/login/+page.svelte
@@ -1,3 +1,19 @@
+<script lang="ts">
+  let credentials = {
+    username: '',
+    password: ''
+  }
+
+  const handleChange = (event: Event) => {
+    const target = event.target as HTMLInputElement
+    const { name, value } = target
+    credentials = {
+      ...credentials,
+      [name]: value
+    }
+  }
+</script>
+
 <div class="flex items-center justify-center min-h-screen p-4">
   <main class="flex flex-col max-w-md w-full gap-4 p-6 bg-gray-800 rounded-lg">
     <header>
@@ -13,6 +29,8 @@
             class="flex w-full h-10 px-3 py-2 bg-gray-700 border-gray-600 text-sm border rounded-md"
             type="text"
             name="username"
+            value={credentials.username}
+            on:input={handleChange}
             required
           />
         </label>
@@ -22,6 +40,8 @@
             class="flex w-full h-10 px-3 py-2 bg-gray-700 border-gray-600 text-sm border rounded-md"
             type="password"
             name="password"
+            value={credentials.password}
+            on:input={handleChange}
             required
           />
         </label>
