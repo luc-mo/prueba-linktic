9dae7ca4100e25224a0465905e6df83a65b25fde
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 13:56:55 -0400

    feat: add parse product form data utility function

--- apps/frontend/src/utils/index.ts
+++ apps/frontend/src/utils/index.ts
@@ -4,3 +4,19 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]): string {
 	return twMerge(clsx(inputs))
 }
+
+export function parseProductFormData(form: FormData): StrictOmit<ProductEntity, 'id'> {
+	const data = Object.fromEntries(form.entries())
+	const name = data.name as string
+	const description = data.description as string
+	const price = data.price as string
+	const stock = data.stock as string
+
+	return {
+		name,
+		description,
+		price: Number.parseInt(price),
+		stock: Number.parseInt(stock),
+		enabled: true,
+	}
+}
