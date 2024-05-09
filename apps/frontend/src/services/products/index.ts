0598ef7f77652609b91108be8be556de7cedd488
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 13:43:07 -0400

    feat: add save product request

--- apps/frontend/src/services/products/index.ts
+++ apps/frontend/src/services/products/index.ts
@@ -1,12 +1,18 @@
 import { HttpClient } from '@/services/http-client'
-import type { GetAllProductsResponse } from './types'
+import type { GetAllProductsResponse, SaveProductRequest } from './types'
 
 export namespace ProductsService {
 	enum Routes {
 		GET_ALL_PRODUCTS = '/v1/products',
+		SAVE_PRODUCT = '/v1/products/:id',
 	}
 
 	export async function getAllProducts() {
 		return HttpClient.get<GetAllProductsResponse>({ url: Routes.GET_ALL_PRODUCTS })
+	}
+
+	export async function saveProduct({ id, ...rest }: SaveProductRequest) {
+		const url = Routes.SAVE_PRODUCT.replace(':id', id)
+		return HttpClient.put<void>({ url, body: rest })
 	}
 }
--- apps/frontend/src/services/products/types.d.ts
+++ apps/frontend/src/services/products/types.d.ts
@@ -1,3 +1,12 @@
 export interface GetAllProductsResponse {
 	products: Product[]
 }
+
+export interface SaveProductRequest {
+	id: string
+	name: string
+	description: string
+	price: number
+	stock: number
+	enabled: boolean
+}
