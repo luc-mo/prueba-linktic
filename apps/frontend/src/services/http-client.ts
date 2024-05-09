26de231a2d2f34a5c49557c1f240c52b9b5f78da
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 07:56:25 -0400

    feat: add http client service

---
+++ apps/frontend/src/services/http-client.ts
@@ -0,0 +1,72 @@
+import axios, { AxiosError, type AxiosRequestConfig } from 'axios'
+import { config } from '@/config/environment'
+import { HttpError, UnexpectedError } from '@/errors'
+
+const _isResponseError = (error: unknown): error is AxiosError => {
+	const isAxiosError = error instanceof AxiosError
+	const hasResponse = isAxiosError && error.response !== undefined
+	return isAxiosError && hasResponse
+}
+
+const _handleError = (error: unknown) => {
+	if (_isResponseError(error)) {
+		throw new HttpError({
+			message: error.message,
+			status: error.response!.status,
+			data: error.response!.data,
+		})
+	}
+	throw new UnexpectedError('An unexpected error occurred')
+}
+
+const _instance = axios.create({
+	baseURL: config.api.baseUrl,
+	headers: {
+		'Content-Type': 'application/json',
+	},
+})
+
+export const HttpClient = {
+	instance: _instance,
+	setAuthToken: (token: string) => {
+		_instance.defaults.headers['auth-token'] = `Bearer ${token}`
+	},
+	post: async <T>({ url, body, options }: Post) => {
+		const { data } = await _instance.post<T>(url, body, options).catch(_handleError)
+		return data
+	},
+	get: async <T>({ url, options }: Get) => {
+		const { data } = await _instance.get<T>(url, options).catch(_handleError)
+		return data
+	},
+	put: async <T>({ url, body, options }: Put) => {
+		const { data } = await _instance.put<T>(url, body, options).catch(_handleError)
+		return data
+	},
+	delete: async <T>({ url, options }: Delete) => {
+		const { data } = await _instance.delete<T>(url, options).catch(_handleError)
+		return data
+	},
+}
+
+interface Post {
+	url: string
+	body?: any
+	options?: AxiosRequestConfig
+}
+
+interface Get {
+	url: string
+	options?: AxiosRequestConfig
+}
+
+interface Put {
+	url: string
+	body?: any
+	options?: AxiosRequestConfig
+}
+
+interface Delete {
+	url: string
+	options?: AxiosRequestConfig
+}
