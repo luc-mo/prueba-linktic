3899cf9bd6c823ee687e38fc029b775ce830b3a2
SnowDrive <luc.moralesr@gmail.com>
2024-05-08 23:17:15 -0400

    feat: expose auth controller at entry point file

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,21 +1,17 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
 import express from 'express'
 import cors from 'cors'
 
-const port = process.env.PORT || 3000
+import { config } from './infrastructure/config'
+import { authController } from './infrastructure/http/auth-controller'
+
+const { port } = config.server
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get('*', (req, res) => {
-	res.send('Hello World')
-})
+app.use('/api/v1/auth', authController)
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`)
 })
-
-export default app
