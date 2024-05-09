a007848f9edcfe493bd5a34bf730ffa402563bde
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 11:42:42 -0400

    feat: forward class name prop from button and link components

--- apps/frontend/src/components/button.svelte
+++ apps/frontend/src/components/button.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import clsx from 'clsx'
+  import { cn } from '@/utils'
+
   type Types = 'button' | 'submit'
   type Variants = 'outlined' | 'contained'
 
@@ -7,13 +9,16 @@
   export let variant: Variants = 'contained'
   export let onClick: (() => void | Promise<void>) | undefined = undefined
 
-  const classes = clsx(
+  let className = ''
+  export { className as class }
+
+  const classes = cn(clsx(
     'flex items-center justify-center h-9 px-3 text-sm font-medium rounded-md transition-colors',
     {
       'bg-gray-700 hover:bg-gray-600': variant === 'contained',
       'bg-transparent border border-gray-600 hover:bg-gray-700': variant === 'outlined'
     }
-  )
+  ), className)
 </script>
 
 <button
--- apps/frontend/src/components/link.svelte
+++ apps/frontend/src/components/link.svelte
@@ -1,21 +1,26 @@
 <script lang="ts">
   import clsx from 'clsx'
+  import { cn } from '@/utils'
+
   type Variants = 'outlined' | 'contained'
 
   export let href: string
   export let external = false
   export let variant: Variants = 'contained'
 
+  let className = ''
+  export { className as class }
+
   $: target = external ? '_blank' : undefined
   $: rel = external ? 'noopener noreferrer' : undefined
 
-  const classes = clsx(
+  const classes = cn(clsx(
     'flex items-center justify-center h-9 px-3 text-sm font-medium rounded-md transition-colors',
     {
       'bg-gray-700 hover:bg-gray-600': variant === 'contained',
       'bg-transparent border border-gray-600 hover:bg-gray-700': variant === 'outlined'
     }
-  )
+  ), className)
 </script>
 
 <a
