6ee38bd57ce5ec81b913e3679a04ea734f3bd339
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 11:11:26 -0400

    feat: add link component

---
+++ apps/frontend/src/components/link.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import clsx from 'clsx'
+  type Variants = 'outlined' | 'contained'
+
+  export let href: string
+  export let external = false
+  export let variant: Variants = 'contained'
+
+  $: target = external ? '_blank' : undefined
+  $: rel = external ? 'noopener noreferrer' : undefined
+
+  const classes = clsx(
+    'flex items-center justify-center h-9 px-3 text-sm font-medium rounded-md transition-colors',
+    {
+      'bg-gray-700 hover:bg-gray-600': variant === 'contained',
+      'bg-transparent border border-gray-600 hover:bg-gray-700': variant === 'outlined'
+    }
+  )
+</script>
+
+<a
+  class={classes}
+  href={href}
+  target={target}
+  rel={rel}
+>
+  <slot/>
+</a>
