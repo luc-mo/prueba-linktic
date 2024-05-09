746a152d48b3ea8885710b09f711b1e61a29d6b9
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 16:37:05 -0400

    feat: add auth token middleware to save order endpoint, replace user id from body to user request prop

--- apps/backend/src/infrastructure/http/orders-controller.ts
+++ apps/backend/src/infrastructure/http/orders-controller.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 
 import { container } from '@/container'
+import { authTokenMiddleware } from './middleware/auth-token-middleware'
 import { SaveOrderCommand } from '@/application/save-order'
 
 const ordersController = Router()
 
-ordersController.put('/:id', async (req, res) => {
+ordersController.put('/:id', authTokenMiddleware, async (req, res) => {
 	try {
 		const { id } = req.params
-		const { userId, products, shipped } = req.body
+		const userId = req.user?.id
+		const { products, shipped } = req.body
 		const command = new SaveOrderCommand({ id, userId, products, shipped })
 		const saveOrder = container.resolve('saveOrder')
 		await saveOrder.execute(command)
