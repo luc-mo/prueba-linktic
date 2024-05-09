e5e017c31ae31031b63ca0161faf36f802da80a5
SnowDrive <luc.moralesr@gmail.com>
2024-05-09 10:10:58 -0400

    feat: add button component

---
+++ apps/frontend/src/components/button.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import clsx from 'clsx'
+  type Types = 'button' | 'submit'
+  type Variants = 'outlined' | 'contained'
+
+  export let type: Types = 'button'
+  export let variant: Variants = 'contained'
+  export let onClick: () => void | Promise<void>
+
+  const classes = clsx(
+    'flex items-center justify-center h-9 px-3 text-sm font-medium rounded-md transition-colors',
+    {
+      'bg-gray-700 hover:bg-gray-600': variant === 'contained',
+      'bg-transparent border border-gray-600 hover:bg-gray-700': variant === 'outlined'
+    }
+  )
+</script>
+
+<button
+  class={classes}
+  type={type}
+  on:click={onClick}
+>
+  <slot/>
+</button>
